92f9a10a1e9a702a90405c40a971493c82248d4c
Sergey Deev <mygrayroom@gmail.com>
2023-06-06 20:43:38 +0300

    Start maven project with "mvn archetype:generate" command

---
+++ src/main/java/com/grayroom/App.java
@@ -0,0 +1,13 @@
+package com.grayroom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
