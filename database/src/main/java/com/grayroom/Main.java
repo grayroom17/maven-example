f6dc1106ecd70f28059d2710ca91d0ead0c736fe
DeevS <deev@advantum.pro>
2023-06-09 16:55:08 +0300

    Change to multimodal project

---
+++ database/src/main/java/com/grayroom/Main.java
@@ -0,0 +1,7 @@
+package com.grayroom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/com/grayroom/MyServlet.java
+++
@@ -1,6 +0,0 @@
-package com.grayroom;
-
-import jakarta.servlet.http.HttpServlet;
-
-public class MyServlet extends HttpServlet {
-}
