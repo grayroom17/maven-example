b6917d51b6086552c125b2e3ca328ea5bdee6a5a
DeevS <deev@advantum.pro>
2023-06-09 12:07:47 +0300

    Failsafe plugin and integration tests

---
+++ src/test/java/com/grayroom/it/ExampleIT.java
@@ -0,0 +1,12 @@
+package com.grayroom.it;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+}
